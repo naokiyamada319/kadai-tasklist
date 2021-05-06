5523abaacffd7f1209828eb3ea8d42ee2a38d023
naoki.yamada <nkymd2018@gmail.com>
2021-05-06 19:38:43 +0900

    Add validation

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -41,17 +43,24 @@ public class UpdateServlet extends HttpServlet {
 			Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 			t.setUpdated_at(currentTime);
 
-			String content = request.getParameter("content");
-			t.setContent(content);
+			List<String> errors = TaskValidator.validate(t);
+			if(errors.size() > 0) {
+				em.close();
 
-			em.getTransaction().begin();
-			em.getTransaction().commit();
-			request.getSession().setAttribute("flush", "更新が完了しました。");
-			em.close();
+				request.setAttribute("_token", request.getSession().getId());
+				request.setAttribute("task", t);
+				request.setAttribute("errors", errors);
+			} else {
 
-			request.getSession().removeAttribute("task_id");
+				em.getTransaction().begin();
+				em.getTransaction().commit();
+				request.getSession().setAttribute("flush", "更新が完了しました。");
+				em.close();
 
-			response.sendRedirect(request.getContextPath() + "/index");
+				request.getSession().removeAttribute("task_id");
+
+				response.sendRedirect(request.getContextPath() + "/index");
+			}
 		}
 	}
 }
